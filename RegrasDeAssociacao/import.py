641e9401e3961d8095675de2373515f0b2bdb365
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-01-15 16:04:37 -0200

    Continuando

--- RegrasDeAssociacao/import.py
+++ RegrasDeAssociacao/import.py
@@ -3,3 +3,12 @@
 import pandas as pd
 
 dados = pd.read_csv("mercado.csv", header=None)
+
+transacoes = [0]
+for i in range(0, 10):
+    transacoes.append([str(dados.values[i, j]) for j in range(0, 4)])
+
+from apyori import apriori
+regras = apriori(transacoes)
+
+resultados = list(regras)
