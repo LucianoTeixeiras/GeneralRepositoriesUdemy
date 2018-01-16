4fa8c2d8f844007d991e61864cc732395d618370
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-01-15 22:44:20 -0200

    Commit 2244

--- RegrasDeAssociacao/mercado.py
+++ RegrasDeAssociacao/mercado.py
@@ -1,9 +1,13 @@
-# Luciano
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 18:13:05 2018
+
+@author: luciano.silva
+"""
 
 import pandas as pd
 
 dados = pd.read_csv('mercado.csv', header=None)
-
 transacoes = []
 for i in range(0,10):
     transacoes.append([str(dados.values[i,j]) for j in range(0,4)])
