6771009cabe1b576cb756b7a0dcb69e7431ff2a1
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-04-30 16:47:59 -0300

    Commit novamente

---
+++ AlgoritmosEmJava/src/pkg_01/Main.java
@@ -0,0 +1,38 @@
+package pkg_01;
+
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double x = 10.35784;
+		int y = 32;
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+
+		System.out.println("Teste Inicial!!!");
+
+		System.out.println("Teste Inicial 1!!!");
+
+		System.out.println(x);
+
+		System.out.printf("%.2f%n", x);
+
+		Locale.setDefault(new Locale("pt", "BR"));
+
+		System.out.printf("%.2f%n", x);
+
+		System.out.println(y);
+
+		System.out.println("O Valor de x é: " + x);
+
+		System.out.printf("O Valor de x é: %.2f%n", x);
+
+		System.out.printf("%s tem %d anos e recebe R$ %.2f Reais%n", nome, idade, renda);
+
+	}
+
+}
