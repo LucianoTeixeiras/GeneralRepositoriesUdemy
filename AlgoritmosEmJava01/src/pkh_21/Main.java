604e836d5624b1f55c3a7634081d64e9e6c509e4
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-02 13:15:18 -0300

    Commit

--- AlgoritmosEmJava01/src/pkh_21/Main.java
+++ AlgoritmosEmJava01/src/pkh_21/Main.java
@@ -1,4 +1,4 @@
-package pkg_21;
+package pkh_21;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -7,24 +7,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 		sc.useLocale(Locale.ENGLISH);
 		Locale.setDefault(new Locale("en", "US"));
-		
+
 		double base, altura, area, perimetro, diagonal;
-		
+
 		base = sc.nextDouble();
 		altura = sc.nextDouble();
-		
+
 		area = base * altura;
 		perimetro = 2.0 * (base + altura);
 		diagonal = Math.sqrt(Math.pow(base, 2.0) + Math.pow(altura, 2.0));
-		
+
 		System.out.printf("AREA = %.4f%n", area);
 		System.out.printf("PERIMETRO = %.4f%n", perimetro);
 		System.out.printf("DIAGONAL = %.4f%n", diagonal);
-		
+
 		sc.close();
 
 	}
