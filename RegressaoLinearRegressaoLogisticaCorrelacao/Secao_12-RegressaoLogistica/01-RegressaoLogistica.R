274fb915ce571e1cac3f6eb7ad06f5ca43750dde
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-06-13 14:08:08 -0300

    Commit Final

--- RegressaoLinearRegressaoLogisticaCorrelacao/Secao_12-RegressaoLogistica/01-RegressaoLogistica.R
+++ RegressaoLinearRegressaoLogisticaCorrelacao/Secao_12-RegressaoLogistica/01-RegressaoLogistica.R
@@ -1,2 +1,36 @@
 # Regressão Logística
 
+# Importando Dataset
+
+library(readr)
+eleicao <- read_delim("Eleicao.csv", ";", 
+                      escape_double = FALSE, trim_ws = TRUE)
+View(eleicao)
+
+head(eleicao)
+
+plot(eleicao$DESPESAS, eleicao$SITUACAO)
+
+modelo = glm(SITUACAO ~ DESPESAS, data = eleicao, family = "binomial")
+
+modelo
+
+summary(modelo)
+
+# Valor ajustado
+modelo$fitted.values
+
+plot(eleicao$DESPESAS, eleicao$SITUACAO, col='red', pch=20 )
+
+points(eleicao$DESPESAS, modelo$fitted, pch=4)
+
+prevereleicao <- read_delim("NovosCandidatos.csv", ";", 
+                      escape_double = FALSE, trim_ws = TRUE)
+
+prevereleicao
+
+prevereleicao$RESULTADO = predict(modelo,newdata = prevereleicao, type = "response")
+
+prevereleicao$RESULTADO
+
+prevereleicao
