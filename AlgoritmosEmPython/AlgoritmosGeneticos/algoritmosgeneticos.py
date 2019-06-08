8bb020e34a798228e658e0a97bb5c5940fd4025b
Luciano Teixeira <luciano.teixeiras@gmail.com>
2019-06-08 15:25:42 -0300

    commit inicial
    Commit inicial de Algorímos:
    Genéticos
    Inteligentes
    Oimização

---
+++ AlgoritmosEmPython/AlgoritmosGeneticos/algoritmosgeneticos.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 15:22:29 2019
+
+@author: Luciano
+"""
+
---
+++ AlgoritmosEmPython/AlgoritmosInteligentes/algoritmosinteligentes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 15:23:19 2019
+
+@author: Luciano
+"""
+
+print("Teste")
